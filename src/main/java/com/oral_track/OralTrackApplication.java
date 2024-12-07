17afe0329bad172a9a29e5b517bfe32bc19b5210
Breno Duarte <brenoo.duarte@hotmail.com>
2024-12-07 15:57:35 -0300

    initial setup

---
+++ src/main/java/com/oral_track/OralTrackApplication.java
@@ -0,0 +1,13 @@
+package com.oral_track;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OralTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OralTrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oral_track/OralTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oral_track;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OralTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
